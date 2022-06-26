d0c15061e3687e030b3617ec543bc29ac048338b
igor2000xp <igorring@gmail.com>
2022-06-26 23:15:13 +0300

    feat: implement circle

--- src/controllers/drawCommands/drawCircle.ts
+++ src/controllers/drawCommands/drawCircle.ts
@@ -1,10 +1,7 @@
 import * as robot from 'robotjs';
 
 export const drawCircle = (x:number,y:number, rad:number) => {
-  const _x = x + rad;
-  const _y = y;
-
-  robot.dragMouse(_x, _y);
+  robot.dragMouse(x + rad, y);
   robot.mouseToggle('down');
 
   for (let i = 0; i <= rad * Math.PI * 2; i += 1) {
@@ -14,6 +11,5 @@ export const drawCircle = (x:number,y:number, rad:number) => {
   }
 
   robot.dragMouse(x, y);
-
   robot.mouseToggle('up')
 };
